bc072ea6c150ecfe636fd7e1a7ed54cbf57a92b1
Ritik <ritikverma0986@gmail.com>
2021-05-22 16:31:19 +0530

    Location Permission on development

--- app/src/main/java/com/example/covs/HomeBasic.kt
+++ app/src/main/java/com/example/covs/HomeBasic.kt
@@ -1,13 +1,52 @@
 package com.example.covs
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class HomeBasic : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_basic)
+        val button: Button = findViewById(R.id.button)
+        val text: TextView = findViewById(R.id.textView2)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        button.setOnClickListener{
+            check_per()
+        }
+    }
+
+    private fun check_per() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+            }
+            shouldShowRequestPermissionRationale(...) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            showInContextUI(...)
+        }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+        }
+
     }
 }
