ce48af90a8c85c9c4d7d8566bd9a9ec78b6e6621
Ritik <ritikverma0986@gmail.com>
2021-05-22 13:24:22 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/covs/HomeBasic.kt
@@ -0,0 +1,13 @@
+package com.example.covs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class HomeBasic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_basic)
+    }
+}
