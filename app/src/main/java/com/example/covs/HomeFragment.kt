222627568c5e597bbcf31ba4112d2d2b05bf6704
Amit <amitkoranga2@gmail.com>
2021-05-24 18:53:23 +0530

    first page ui prototype

--- app/src/main/java/com/example/covs/HomeFragment.kt
+++ app/src/main/java/com/example/covs/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.covs
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import app.futured.donut.DonutSection
+
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
@@ -13,8 +17,30 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        view.donut_view.cap = 1f
+        view.donut_view.submitData(getSections())
+        return view
+    }
+
+    private fun getSections(): List<DonutSection>{
+        return listOf(
+            DonutSection(
+                name = "Active",
+                color = Color.parseColor("#FCEC52"),
+                amount = 1f
+            ),
+            DonutSection(
+                name = "Recovered",
+                color = Color.parseColor("#058C42"),
+                amount = 2f
+            ),
+            DonutSection(
+                name = "Death",
+                color = Color.parseColor("#FF0000"),
+                amount = 1f
+            )
+        )
     }
 
 }
