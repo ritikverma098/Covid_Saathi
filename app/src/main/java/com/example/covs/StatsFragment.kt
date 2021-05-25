18b469ec1ce322bc66250595ce7c692067ef2416
Amit <amitkoranga2@gmail.com>
2021-05-25 15:05:09 +0530

    stats fragment

---
+++ app/src/main/java/com/example/covs/StatsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.covs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_stats.view.*
+
+class StatsFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_stats, container, false)
+
+        return view
+    }
+
+}
